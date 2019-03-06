24d35177ea988ad4d7e729c4f4da12d464008a5c
rogday <s.e.a.98@yandex.ru>
2019-03-06 16:48:39 +0000

    Tests fix

--- src/tests/bridges.cpp
+++ src/tests/bridges.cpp
@@ -25,24 +25,29 @@ void estimate(Graph &g, std::string_view name, Functor f, bool show) {
 }
 
 int main() {
-	for (std::uint64_t n = 10; n < 1'000'000; n *= 10) {
-		std::uint64_t m = 2 * n;
+	for (std::uint64_t i = 0; i < 2; ++i) {
+		std::cout << ((i) ? "Recursive" : "Iterative") << std::endl;
+		for (std::uint64_t n = 10; n < 1'000'000; n *= 10) {
+			std::uint64_t m = 2 * n;
 
-		std::cout << n << " " << m << std::endl;
+			std::cout << n << " " << m << std::endl;
 
-		Graph g(n, m);
+			Graph g(n, m);
 
-		g.random_bridges_search(); // just to be fair in terms of cache,
-								   // everything is clearing before each
-								   // algorithm
+			g.random_bridges_search(i); // just to be fair in terms of cache,
+										// everything is clearing before each
+										// algorithm
 
-		estimate(g, "Random", std::bind(&Graph::random_bridges_search, _1),
-				 false);
-		estimate(g, "Determenistic",
-				 std::bind(&Graph::determined_bridges_search, _1), false);
-		estimate(g, "Random two-bridges",
-				 std::bind(&Graph::random_two_bridges_search, _1), false);
+			estimate(g, "Random",
+					 std::bind(&Graph::random_bridges_search, _1, i), false);
+			estimate(g, "Determenistic",
+					 std::bind(&Graph::determined_bridges_search, _1, i),
+					 false);
+			estimate(g, "Random two-bridges",
+					 std::bind(&Graph::random_two_bridges_search, _1, i),
+					 false);
 
-		std::cout << std::endl;
+			std::cout << std::endl;
+		}
 	}
 }
